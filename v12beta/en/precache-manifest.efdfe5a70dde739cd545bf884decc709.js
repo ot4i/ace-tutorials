099349c55db95a8d949fbdd319088e0abbef893c
Ben Thompson <bthomps@uk.ibm.com>
2021-02-08 13:46:00 +0000

    Fix Start

---
+++ v12beta/en/precache-manifest.efdfe5a70dde739cd545bf884decc709.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963837cc8d8856e6442d824b050450f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8361fc0be10b45b21bc",
+    "url": "./static/css/main.0627184d.chunk.css"
+  },
+  {
+    "revision": "d8d61f62e961bdaf797b",
+    "url": "./static/js/2.20a2e574.chunk.js"
+  },
+  {
+    "revision": "c8361fc0be10b45b21bc",
+    "url": "./static/js/main.4a10e074.chunk.js"
+  },
+  {
+    "revision": "0064dd6e05b111a4b2be",
+    "url": "./static/js/runtime-main.a1c7eda6.js"
+  },
+  {
+    "revision": "5e4ff69e532736a254fbfbc5000c5d05",
+    "url": "./static/media/api-error.lg.5e4ff69e.svg"
+  },
+  {
+    "revision": "22860dd00c8dee4bce1b74a9ff43f3b3",
+    "url": "./static/media/api-error.sm.22860dd0.svg"
+  },
+  {
+    "revision": "2fd637471ca83a3063641ec9b08cff39",
+    "url": "./static/media/cics-icon.2fd63747.svg"
+  },
+  {
+    "revision": "1df1faf1b0365bd7bff1914f76bdb754",
+    "url": "./static/media/default.lg.1df1faf1.svg"
+  },
+  {
+    "revision": "80b8aaa83a8d90090014eb14cff79bc3",
+    "url": "./static/media/default.sm.80b8aaa8.svg"
+  },
+  {
+    "revision": "62817230c07efd15dab978b729bc0e7d",
+    "url": "./static/media/deploy.lg.62817230.svg"
+  },
+  {
+    "revision": "6af3724c0f31af54a766c5984a693a66",
+    "url": "./static/media/deploy.sm.6af3724c.svg"
+  },
+  {
+    "revision": "b1a9e55c6ffd01a5a8171096dc2de9b3",
+    "url": "./static/media/icon-tree-file.b1a9e55c.svg"
+  },
+  {
+    "revision": "36db6ff004e0c57d3bbb3dc3e6a4c04b",
+    "url": "./static/media/icons-node-http.36db6ff0.svg"
+  },
+  {
+    "revision": "94ee2aad7e36f158f7241c0489cdc0c5",
+    "url": "./static/media/icons.94ee2aad.svg"
+  },
+  {
+    "revision": "77fc5a1e9b1db0a54c4d0a5e1138b3a4",
+    "url": "./static/media/mq.77fc5a1e.svg"
+  },
+  {
+    "revision": "d18c50ff835913d5230dfee527a7d3c9",
+    "url": "./static/media/no-search-results.lg.d18c50ff.svg"
+  },
+  {
+    "revision": "bc3f8989d98d95b6468922d2965619df",
+    "url": "./static/media/no-search-results.sm.bc3f8989.svg"
+  },
+  {
+    "revision": "0590c04c4924dcf35245eab19aca08c9",
+    "url": "./static/media/not-authorized.lg.0590c04c.svg"
+  },
+  {
+    "revision": "47783dd522cadef2c2d9886c2947cdc8",
+    "url": "./static/media/not-authorized.sm.47783dd5.svg"
+  }
+]);
--- v12beta/en/service-worker.js
+++ v12beta/en/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.117dbfd39507e42399f79c4353b9fae3.js"
+  "./precache-manifest.efdfe5a70dde739cd545bf884decc709.js"
 );
 
 self.addEventListener('message', (event) => {
