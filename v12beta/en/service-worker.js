5d6773701630c69341759506df7ee47d6acc000b
Kuhan Loh <kuhan.loh1@ibm.com>
2021-03-10 12:37:57 +0000

    Update v12beta (Kuhan and Ben build 23, localStorage refresh)

--- v12beta/en/service-worker.js
+++ v12beta/en/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3a52565c95240cdec25ee92a480b6dc2.js"
+  "./precache-manifest.bcc71ad74e6c76b3760155e0fc724656.js"
 );
 
 self.addEventListener('message', (event) => {
