018a970c255a1955c66f543b05905cf9b8731546
Ben Thompson <bthomps@uk.ibm.com>
2021-03-16 13:49:23 +0000

    Update v12

--- v12/en/service-worker.js
+++ v12/en/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3a52565c95240cdec25ee92a480b6dc2.js"
+  "./precache-manifest.d9ea904ffd922a86c03b4d239b839652.js"
 );
 
 self.addEventListener('message', (event) => {
